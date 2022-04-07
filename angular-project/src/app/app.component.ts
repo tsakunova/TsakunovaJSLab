78b276d41ca3f7eff6f5ff1ed86a9cfb5bde67e1
Tatsiana Tsakunova <tv.tsakunova@gmail.com>
2022-04-08 02:22:11 +0300

    feat: add test component

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-project';
+  title = 'Dynamic title - interpolation'
+  obj = {
+    a:1,
+    b:2
+  }
 }
