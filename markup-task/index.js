d1b6f4c56fb3317a53dc085f612dd56d9f05147f
Tatsiana Tsakunova <tv.tsakunova@gmail.com>
2022-04-15 03:35:22 +0300

    feat: add animation and hover effect; fix: class names, svg icons

---
+++ markup-task/index.js
@@ -0,0 +1,39 @@
+const animateBlocks = document.querySelectorAll('.animate-block');
+
+if(animateBlocks.length>0){
+  window.addEventListener('scroll', animateScroll);
+  function animateScroll(){
+    for(let i = 0; i< animateBlocks.length; i++){
+      const animItem = animateBlocks[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 8;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if(animItemHeight > window.innerHeight){
+        animItemPoint = window.innerHeight - window.innerHeight/animStart;
+      }
+    
+    if((pageYOffset>animItemOffset - animItemPoint) && pageYOffset<(animItemOffset+animItemHeight)){
+      animItem.classList.add('active')
+    } else {
+      if(!animItem.classList.contains('animate-block__no-hide')){
+        animItem.classList.remove('active')
+      }
+      
+    }
+  }
+}
+
+function offset(el){
+  const rect = el.getBoundingClientRect(),
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return {
+    top: rect.top + scrollTop,
+    left: rect.left +scrollLeft
+  }
+}
+  animateScroll();
+}
